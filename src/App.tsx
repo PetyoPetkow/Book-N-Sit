7e4a273b2b01252157c496592ab93da120cf20eb
Petyo Petkov <Pspetkov13@gmail.com>
2024-05-11 16:11:16 +0300

    Make Card Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
-import UserAccountPage from './pages/users/UserAccountPage'
-import Header from './navigation/Header'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import UserAccountPage from './pages/users/UserAccountPage';
+import Header from './navigation/Header';
+import Places from './pages/places/Places';
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Places />} />
         <Route path="/account" element={<UserAccountPage />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/places/Places.tsx
@@ -0,0 +1,87 @@
+import { FC } from 'react';
+import { Card } from 'antd';
+
+const Places: FC<PlacesProps> = () => {
+  const { Meta } = Card;
+
+  return (
+    <>
+      <div className="flex flex-wrap justify-center w-full gap-10">
+        {mock.map(({ title, address, workingTime, src }) => {
+          return (
+            <Card
+              key={title}
+              className="w-80"
+              hoverable
+              cover={<img className="h-60" alt="example" src={src} />}
+            >
+              <Meta title={title} description={address} />
+              <div>{workingTime}</div>
+            </Card>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+interface PlacesProps {}
+
+const mock = [
+  {
+    title: 'Bianco',
+    address: 'Велико Търново Център, пл. „Майка България“ 1, 5000 Велико Търново',
+    workingTime: '08:00 - 00:00',
+    src: 'https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg',
+  },
+  {
+    title: 'Lino Bar',
+    address: 'Veliko Tarnovo, Nezavisimost St 3',
+    workingTime: '08:00 - 00:00',
+    src: 'https://c.ndtvimg.com/2023-11/c4bp49g_restaurant-generic_625x300_21_November_23.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=738',
+  },
+  {
+    title: 'Стратилат',
+    address: 'ж.к. Варуша-север, ул. „Георги С. Раковски“ 11, 5000 Велико Търново',
+    workingTime: '08:00 - 20:00',
+    src: 'https://assets.vogue.com/photos/618e7c4badd0a25be01d750e/master/w_2560%2Cc_limit/GettyImages-1222654885.jpg',
+  },
+  {
+    title: 'Bianco',
+    address: 'Велико Търново Център, пл. „Майка България“ 1, 5000 Велико Търново',
+    workingTime: '08:00 - 00:00',
+    src: 'https://www.viajoteca.com/wp-content/uploads/2015/03/Cafe-de-Flore-Capa.jpg',
+  },
+  {
+    title: 'Lino Bar',
+    address: 'Veliko Tarnovo, Nezavisimost St 3',
+    workingTime: '08:00 - 00:00',
+    src: 'https://www.foodandwine.com/thmb/8rtGtUmtC0KiJCDxAUXP_cfwgM8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GTM-Best-US-Bars-Katana-Kitten-FT-BLOG0423-fa9f2ba9925c47abb4afb0abd25d915a.jpg',
+  },
+  {
+    title: 'Стратилат',
+    address: 'ж.к. Варуша-север, ул. „Георги С. Раковски“ 11, 5000 Велико Търново',
+    workingTime: '08:00 - 20:00',
+    src: 'https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg',
+  },
+  {
+    title: 'Bianco',
+    address: 'Велико Търново Център, пл. „Майка България“ 1, 5000 Велико Търново',
+    workingTime: '08:00 - 00:00',
+    src: 'https://i.pinimg.com/736x/b4/01/dc/b401dc64405c4d9de4f1cf8afc412136.jpg',
+  },
+  {
+    title: 'Lino Bar',
+    address: 'Veliko Tarnovo, Nezavisimost St 3',
+    workingTime: '08:00 - 00:00',
+    src: 'https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg',
+  },
+  {
+    title: 'Стратилат',
+    address: 'ж.к. Варуша-север, ул. „Георги С. Раковски“ 11, 5000 Велико Търново',
+    workingTime: '08:00 - 20:00',
+    src: 'https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg',
+  },
+];
+
+export default Places;
